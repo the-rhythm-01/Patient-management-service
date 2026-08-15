f68f1c5f91e46a5b0b83f2a143626544c47a2d99
Ritam <ritammondal.rj2001@gmail.com>
2026-08-15 22:31:58 +0530

    Added billing service- proto, grpc, docker file etc

---
+++ Patient-management/billing-service/src/main/java/com/ritam/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.ritam.billingservice.grpc;
+
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver){
+        log.info("Create Billing Account request received {}", billingRequest);
+
+        //Some business logic
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("Active")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
